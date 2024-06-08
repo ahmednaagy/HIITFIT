6019acef06da4821d7850748ca38ddf99dd10668
Ahmed Nagy <ahmed.nagy@souhoola.com>
2024-06-08 22:26:25 +0300

    Initial Commit

---
+++ HIITFIT/HIITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFITApp.swift
+//  HIITFIT
+//
+//  Created by Ahmed Nagy on 08/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HIITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
