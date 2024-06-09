8c85c8e76a0dd2b06456603168616a8d04358fc1
Ahmed Nagy <ahmed.nagy@souhoola.com>
2024-06-09 23:34:32 +0300

    feat: Implemented tab navigation for improved user experience

--- HIITFIT/ContentView.swift
+++ HIITFIT/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        TabView {
+            Text("Welcome")
+            Text("Exercise 1")
+            Text("Exercise 2")
         }
-        .padding()
+
     }
 }
 
