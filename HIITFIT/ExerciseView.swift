2daadf08b2a33ab3fbd359795a3c6017f7e9103e
Ahmed Nagy <ahmednaagy97@gmail.com>
2024-11-29 08:34:43 +0200

    Add ExerciseView with video playback and timer functionality

--- HIITFIT/ExerciseView.swift
+++ HIITFIT/ExerciseView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
 
@@ -15,14 +16,33 @@ struct ExerciseView: View {
 		Exercise.exercises[index]
 	}
 
+	let interval: TimeInterval = 30.0
+
 	var body: some View {
-		VStack {
-			HeaderView(exerciseName: exercise.exerciseName)
-			Text("Video player")
-			Text("Timer")
-			Text("Start/Done button")
-			Text("Rating")
-			Text("History button")
+		GeometryReader { geometry in
+			VStack {
+				HeaderView(exerciseName: exercise.exerciseName)
+					.padding(.bottom)
+				if let url = Bundle.main.url(
+					forResource: exercise.videoName,
+					withExtension: "mp4"
+				) {
+					VideoPlayer(player: AVPlayer(url: url))
+						.frame(height: geometry.size.height * 0.45)
+				} else {
+					Text("Could't find \(exercise.videoName).mp4")
+						.foregroundStyle(.red)
+				}
+				Text(Date().addingTimeInterval(interval), style: .timer)
+					.font(.system(size: geometry.size.height * 0.07))
+				Button("Start/Done") { }
+					.font(.title3)
+					.padding()
+				Text("Rating")
+				Spacer()
+				Button("History") { }
+					.padding(.bottom)
+			}
 		}
 	}
 }
